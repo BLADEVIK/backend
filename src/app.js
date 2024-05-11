5c7129f53cf411b08cbd9b3e61c2399d6a2bd82b
Viktor Pirogov <Blade-85@inbox.ru>
2024-05-11 15:51:30 +0300

    Routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
+// const userRouter = require("./routes/users");
 dotenv.config();
 
 const app = express();
-const { PORT, API_URL = 'http://127.0.0.1' } = process.env;
+const { PORT, API_URL = "http://127.0.0.1" } = process.env;
 console.log(process.env.PORT);
-app.get("/", (request, response) => {
+const helloWorld = (request, response) => {
   response.status(200);
   response.send("Hello World!");
+};
+app.get("/", helloWorld);
+app.get("/users/34", (request, response) => {
+  response.status(200);
+  response.send("User with id: 34");
 });
+app.post("/", (request, response) => {
+  response.status(200);
+  response.send("Hello POST!");
+});
+// app.use(userRouter);
 app.listen(PORT, () => {
   console.log(`Сервер запущен по адрессу ${API_URL}:${PORT}`);
 });
---
+++ src/contllers/users.js
@@ -0,0 +1,16 @@
+const getUsers = (request, response) => {};
+const getUser = (request, response) => {
+  const { user_id } = request.params;
+  response.status(200);
+  response.send(`User with id: ${user_id} `);
+};
+const createUser = (request, response) => {};
+const updateUser = (request, response) => {};
+const deleteUser = (request, response) => {};
+module.exports = {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+};
